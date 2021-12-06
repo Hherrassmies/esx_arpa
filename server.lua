5e6ddc7aadb8c58c128fe55903a8f408a36b4685
Hherrassmies <94046724+Hherrassmies@users.noreply.github.com>
2021-12-06 21:22:46 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,16 +1,1 @@
-ESX.RegisterUsableItem('raaputusarpa', function(source) 
-    local source = tonumber(source)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local cash = math.random(150, 500)
-    local chance = math.random(1,50)
 
-    if chance == 2 then
-        TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'Voitit $' .. cash .. ' raaputusarvasta'})
-        xPlayer.addMoney(cash)
-        end
-    else
-        TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'Et voittanut mitään.'})
-    end
-
-    xPlayer.removeInventoryItem('raaputusarpa', 1)
-end)
